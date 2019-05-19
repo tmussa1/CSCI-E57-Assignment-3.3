543454eaa04dceb9ccf9c5896f6f0a7743da8ac1
Tofik Mussa <tofahub@gmail.com>
2019-05-19 19:22:28 -0400

    Corrected base url

--- zipkin/src/main/docker/run.sh
+++ zipkin/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 
 echo "*******************************************"
 echo "ZIpkin has started ************************"
-java -Dserver.port=$SERVER_PORT -Dspring.profiles.active=$PROFILE -Dspring.zipkin.baseUrl=$ZIPKIN_URI -jar /usr/local/zipkin/@project.build.finalName@.jar
+java -Dserver.port=$SERVER_PORT -Dspring.profiles.active=$PROFILE -Dspring.zipkin.base-url=$ZIPKIN_URI -jar /usr/local/zipkin/@project.build.finalName@.jar
